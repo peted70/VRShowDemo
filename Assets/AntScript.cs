b8862b70955c367f29a346d312e71535560e2978
unknown <peted70@hotmail.com>
2018-04-05 19:24:33 +0100

    initial add

---
+++ Assets/AntScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AntScript : MonoBehaviour
+{
+    Animator anim;
+    bool _isRunning = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Run()
+    {
+        anim.SetTrigger("Run");
+        _isRunning = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_isRunning)
+        {
+            gameObject.transform.Rotate(new Vector3(0.0f, Random.Range(-45.0f, 45.0f), 0.0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isRunning)
+        {
+            // rotate at random but keep the same general direction?
+            transform.localPosition += transform.forward * Time.deltaTime * 6f;
+        }
+    }
+}
---
+++ Assets/GlobalScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GlobalScript : MonoBehaviour
+{
+    public void MakeAllAntsRun()
+    {
+        var ants = FindObjectsOfType<AntScript>();
+        foreach (var ant in ants)
+        {
+            ant.Run();
+        }
+    }
+
+    public void Clone()
+    {
+        StartCoroutine(Clone(10));
+    }
+
+    public IEnumerator Clone(int num = 10)
+    {
+        var ant = FindObjectOfType<AntScript>();
+        for (int i = 0; i < num; i++)
+        {
+            var newAnt = GameObject.Instantiate(ant.gameObject, new Vector3(0, 0, 0), Quaternion.identity);
+            yield return null;
+        }
+    }
+}
